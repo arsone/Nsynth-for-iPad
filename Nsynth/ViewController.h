87cc9871bae4a41f4292201bd47c5f4f47c9b04e
Adam Salberg <adsalberg@gmail.com>
2018-03-14 22:03:53 -0400

    Initial Commit

---
+++ Nsynth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nsynth
+//
+//  Created by Adam Salberg on 3/14/18.
+//  Copyright © 2018 Adam Salberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
